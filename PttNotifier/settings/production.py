5ba0c67cbd0629410f03824845f61cd242edbd92
bustta <bustta80980@gmail.com>
2014-12-30 16:32:32 +0800

    add production config

--- PttNotifier/settings/production.py
+++ PttNotifier/settings/production.py
@@ -3,4 +3,25 @@
 # @Author: bustta
 # @Date:   2014-12-09 23:18:19
 # @Last Modified by:   bustta
-# @Last Modified time: 2014-12-09 23:29:47
+# @Last Modified time: 2014-12-30 16:26:12
+from .base import *
+
+DEBUG = False
+TEMPALTE_DEBUG = False
+
+SECRET_KEY = get_env_var('PTTNOTIFIER_SECRET')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pttnotifier',
+        'USER': get_env_var('PTTNOTIFIER_DB_DEFAULT_USER'),
+        'PASSWORD': get_env_var('PTTNOTIFIER_DB_DEFAULT_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+ALLOW_HOST = ['*']
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
