b9b03798b29f9b2197b90eeada74161b7fcd0821
BalicantaYao <balicanta.yao@gmail.com>
2015-02-15 18:47:02 -0500

    Fix PEP8 issues

--- fabfile.py
+++ fabfile.py
@@ -1,22 +1,24 @@
-# -*- coding:utf-8 -*-  
+# -*- coding:utf-8 -*-
 
 import os
-from fabric.api import run, local, roles, env, cd , sudo
-env.hosts=['128.199.232.167']
+from fabric.api import run, local, roles, env, cd, sudo
+env.hosts = ['128.199.232.167']
 
 env.user = os.environ['PRODUCTION_USER_NAME']
 env.password = os.environ['PRODUCTION_PASSWORD']
-env.port=22
+env.port = 22
+
 
 def pull():
     with cd('/home/bustta/myWebApps/PttNotifier'):
         run('git pull')
 
+
 def restartNginx():
     sudo('service nginx restart')
 
+
 def collectStatic():
     path = "/home/bustta/myWebApps"
- 
     with cd('/home/bustta/myWebApps/PttNotifier'):
         run('. %s/venv/pttnotifier/bin/activate && python manage.py collectstatic --noinput ' % path)
