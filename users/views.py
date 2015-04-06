7cdb5bf505dd3902949327a90991831e64251f92
DinoLai <dinos80152@gmail.com>
2015-04-06 20:04:36 +0800

    fix pep8

--- users/views.py
+++ users/views.py
@@ -3,17 +3,13 @@ from django.contrib.auth.decorators import login_required
 from .models import User
 from .forms import UserForm
 
+
 # Create your views here.
 @login_required
 def profile(request):
-    user = User.objects.get(pk = request.user.id)
+    user = User.objects.get(pk=request.user.id)
     if request.method == 'POST':
         form = UserForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
     return render(request, 'user_profile.html', {'user': user})
-
-#def setProfile(request):
-#    user = user.get(user_id=request.user.id)
-#    user.email_address = ;
-#    return render()
