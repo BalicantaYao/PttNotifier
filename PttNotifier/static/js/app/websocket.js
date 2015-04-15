e9f664de02fe2d487e0894c61d06d96393d73e18
bustta <bustta80980@gmail.com>
2015-04-16 00:20:32 +0800

    add client code for receive push

--- PttNotifier/static/js/app/websocket.js
+++ PttNotifier/static/js/app/websocket.js
@@ -1,5 +1,5 @@
 (function(global){
-    var wsurl = 'ws://buzz3.co:8000';
+    var wsurl = 'ws://buzz3.co';
     var socket = io.connect(wsurl);
 
     socket.on('date', function(data) {
@@ -7,10 +7,23 @@
     });
 
     socket.on('notify', function(data) {
-        $('#notifications-num').text(data.count);
-        $('#notifications-num').css('display', 'block');
-    });
+        console.log(data.notifications);
+        var notifications_pool = localStorage.getItem('buzz3push');
 
+        if(!notifications_pool){
+            notifications_pool = [];
+        }
+        notifications_pool.push(data.notifications);
+        localStorage.setItem('buzz3push', notifications_pool);
+        var pushCount = notifications_pool.length;
+
+        $('#notifications-num').text(pushCount);
+        $('#notifications-num').css('display', 'block');
+
+    });
+    $('#notifications-num').click(function(){
+        $(this).css('display', 'none');
+    });
 
 
 })(this);
