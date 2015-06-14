4081d3d713e7e418954aca5a6362264a2482bc7e
bustta <bustta80980@gmail.com>
2015-06-14 23:43:09 +0800

    add admin for user_profile

---
+++ users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'substituted_mail'
+        )
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- users/models.py
+++ users/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     substituted_mail = models.EmailField()
+
+    def __str__(self):
+        return self.user.username
